fb0b8ecaa6b36619ed93bc5c0a4e5c352ce5f5a5
Mohanad Darwish <mohanad.ahmed.yehia@gmail.com>
2017-12-16 23:21:23 +0200

    Create Lesson2_OddOccurrencesInArray.c

---
+++ Lesson2_OddOccurrencesInArray.c
@@ -0,0 +1,11 @@
+typedef unsigned int U32;
+
+U32 solution(U32 A[], U32 N) {
+    // write your code in C99 (gcc 6.2.0)
+    U32 i=0;
+    U32 ans=0;
+for(i=0;i<N;i++){
+ ans ^=A[i];     
+}
+return ans;
+}
